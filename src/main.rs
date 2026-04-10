a8e5db24de3ba2e1ebb660551f2f810f42c79f55
Dennis Lee nix <dennis.lee@gmx.ch>
2026-04-10 07:45:48 +0200

    fix alteast one operator

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ const INPUT: &str = "3 4 + 6 2 + 8 9 + 4 3 + * * *"; // 6664
 // const INPUT: &str = "3 1 + 7 8 + 9 8 7 + 1 2 1 4 + + 7 + + + + + +"; // 58
 // const INPUT: &str = "3 4 + *";
 // const INPUT: &str = "8 + 9 + 7 * 2 *";
+// const INPUT: &str = "1 2 +";
 
 fn main() {
     let mut step_mode = false;
@@ -26,6 +27,7 @@ fn main() {
 fn fixed_slice(step_mode: bool) {
     let mut stack: [u32; INPUT.len()] = [0; INPUT.len()];
     let mut stack_index = 0;
+    let mut atleast_one_operator = false;
 
     for ch in INPUT.chars() {
         match ch {
@@ -37,6 +39,8 @@ fn fixed_slice(step_mode: bool) {
                 if stack_index < 2 {
                     panic!("Verwerfend")
                 } else {
+                    atleast_one_operator = true;
+
                     let left_index = stack_index - 2;
                     let right_index = stack_index - 1;
                     stack[left_index] = stack[left_index] + stack[right_index];
@@ -48,6 +52,8 @@ fn fixed_slice(step_mode: bool) {
                 if stack_index < 2 {
                     panic!("Verwerfend")
                 } else {
+                    atleast_one_operator = true;
+
                     let left_index = stack_index - 2;
                     let right_index = stack_index - 1;
                     stack[left_index] = stack[left_index] * stack[right_index];
@@ -67,11 +73,17 @@ fn fixed_slice(step_mode: bool) {
         }
     }
 
-    println!("{}", stack[0]);
+    dbg!(stack_index, atleast_one_operator);
+    if stack_index == 1 && atleast_one_operator {
+        println!("{}", stack[0]);
+    } else {
+        panic!("Verwerfend");
+    }
 }
 
 fn my_stack(input: &str, step_mode: bool) {
     let mut stack = MyStack::with_capacity(input.len() / 4);
+    let mut atleast_one_operator = false;
 
     for ch in input.chars() {
         match ch {
@@ -82,6 +94,8 @@ fn my_stack(input: &str, step_mode: bool) {
                 if stack.len() < 2 {
                     panic!("Verwerfend")
                 } else {
+                    atleast_one_operator = true;
+
                     let right = stack.pop().unwrap();
                     let left = stack.pop().unwrap();
                     stack.push(left + right);
@@ -91,6 +105,8 @@ fn my_stack(input: &str, step_mode: bool) {
                 if stack.len() < 2 {
                     panic!("Verwerfend")
                 } else {
+                    atleast_one_operator = true;
+
                     let right = stack.pop().unwrap();
                     let left = stack.pop().unwrap();
                     stack.push(left * right);
@@ -107,5 +123,10 @@ fn my_stack(input: &str, step_mode: bool) {
         }
     }
 
-    println!("{}", stack.pop().unwrap());
+    dbg!(stack.len(), atleast_one_operator);
+    if stack.len() == 1 && atleast_one_operator {
+        println!("{}", stack.pop().unwrap());
+    } else {
+        panic!("Verwerfend");
+    }
 }
